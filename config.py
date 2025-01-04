9ce53e1c7429c692e7c06b4924c2ddcb0462d80a
rizallal <maulanarizalalvani@gmail.com>
2025-01-05 01:46:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Config:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/project_pwp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'your-secret-key-here'
