b2b5c6fcc307d433c7b810bedcf0d51516a1d8c7
rizallal <maulanarizalalvani@gmail.com>
2025-01-05 02:11:57 +0700

    Add files via upload

---
+++ models/user.py
@@ -0,0 +1,36 @@
+from extensions import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)    
+    password = db.Column(db.String(200))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(),onupdate=db.func.now())
+    
+    def data(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'password': self.password,
+        }
+
+        
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    @classmethod
+    def get_all(cls):
+        r = cls.query.all()
+        result = []
+        
+        for i in r:
+            result.append(i.data())
+        return result
+    
+    @classmethod
+    def get_by_id(cls,id):
+        return cls.query.filter(cls.id == id).first()
